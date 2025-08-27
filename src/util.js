c14b986c8c78b305fc15db0c30eb87f259d355c9
Yuri Andreev Jr <andreev.jr@gmail.com>
2025-08-27 13:47:21 +0300

    Added test qrcodes

--- src/util.js
+++ src/util.js
@@ -1,7 +1,7 @@
 import guests from '@/assets/storage/guests.json';
 
 export const fetchGuests = () => {
-    return fetch('https://raw.githubusercontent.com/LavaExcess/Vue-app-and-barcode-scanner/refs/heads/main/src/assets/storage/guests.json')
+    return fetch('https://raw.githubusercontent.com/LavaExcess/Vue-app-and-barcode-scanner/refs/heads/driver/src/assets/storage/guests.json')
         .then(resp => resp.json())
         .catch((err) => {
             console.warn(err);
